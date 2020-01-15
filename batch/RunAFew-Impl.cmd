c4e584ef48428fb30cc15858f49da61e0dc57747
MUBAREKA Sarah <sarah.mubareka@ec.europa.eu>
2020-01-15 10:15:27 +0100

    %EUCS_100mDir% -> %LuisettaDataDir% Adaptation to GeoDMS 7.207 NUTS_RG_01M_2013_3035_LEVL_3

--- batch/RunAFew-Impl.cmd
+++ batch/RunAFew-Impl.cmd
@@ -1,32 +1,34 @@
 set geodmsversion=GeoDms7207
 set pf=%Program Files%
 set luisetta=E:\dev\Luisetta
-rem "C:\Program Files\ObjectVision\GeoDms7207\GeoDmsRun.exe" /LbatchtraceAT.log "E:\dev\Luisetta\cfg\StatusQuo.dms" /runs/Austria/endstate/GeneratedFiles /runs/Austria/simulation_results/landuse/GridData
-rem "C:\Program Files\ObjectVision\GeoDms7207\GeoDmsRun.exe" /LbatchtraceBL.log "E:\dev\Luisetta\cfg\StatusQuo.dms" /runs/BelgLux/endstate/GeneratedFiles /runs/BelLux/simulation_results/landuse/GridData
-rem "C:\Program Files\ObjectVision\GeoDms7207\GeoDmsRun.exe" /LbatchtraceBG.log "E:\dev\Luisetta\cfg\StatusQuo.dms" /runs/Bulgaria/endstate/GeneratedFiles /runs/Bulgaria/simulation_results/landuse/GridData
-rem "C:\Program Files\ObjectVision\GeoDms7207\GeoDmsRun.exe" /LbatchtraceCY.log "E:\dev\Luisetta\cfg\StatusQuo.dms" /runs/Cyprus/endstate/GeneratedFiles /runs/Cyprus/simulation_results/landuse/GridData
-rem "C:\Program Files\ObjectVision\GeoDms7207\GeoDmsRun.exe" /LbatchtraceHR.log "E:\dev\Luisetta\cfg\StatusQuo.dms" /runs/Croatia/endstate/GeneratedFiles /runs/Croatia/simulation_results/landuse/GridData
-"C:\Program Files\ObjectVision\GeoDms7207\GeoDmsRun.exe" /LbatchtraceCZ.log "E:\dev\Luisetta\cfg\StatusQuo.dms" /runs/Czechia/endstate/GeneratedFiles /runs/Czechia/simulation_results/landuse/GridData
-rem "C:\Program Files\ObjectVision\GeoDms7207\GeoDmsRun.exe" /LbatchtraceDK.log "E:\dev\Luisetta\cfg\StatusQuo.dms" /runs/Denmark/endstate/GeneratedFiles /runs/Denmark/simulation_results/landuse/GridData
-rem "C:\Program Files\ObjectVision\GeoDms7207\GeoDmsRun.exe" /LbatchtraceEE.log "E:\dev\Luisetta\cfg\StatusQuo.dms" /runs/Estonia/endstate/GeneratedFiles /runs/Estonia/simulation_results/landuse/GridData
-rem "C:\Program Files\ObjectVision\GeoDms7207\GeoDmsRun.exe" /LbatchtraceFI.log "E:\dev\Luisetta\cfg\StatusQuo.dms" /runs/Finland/endstate/GeneratedFiles /runs/Finland/simulation_results/landuse/GridData
-rem "C:\Program Files\ObjectVision\GeoDms7207\GeoDmsRun.exe" /LbatchtraceFR.log "E:\dev\Luisetta\cfg\StatusQuo.dms" /runs/France/endstate/GeneratedFiles /runs/France/simulation_results/landuse/GridData
-rem "C:\Program Files\ObjectVision\GeoDms7207\GeoDmsRun.exe" /LbatchtraceDE.log "E:\dev\Luisetta\cfg\StatusQuo.dms" /runs/Germany/endstate/GeneratedFiles /runs/Germany/simulation_results/landuse/GridData
-rem "C:\Program Files\ObjectVision\GeoDms7207\GeoDmsRun.exe" /LbatchtraceEL.log "E:\dev\Luisetta\cfg\StatusQuo.dms" /runs/Greece/endstate/GeneratedFiles /runs/Greece/simulation_results/landuse/GridData
-rem "C:\Program Files\ObjectVision\GeoDms7207\GeoDmsRun.exe" /LbatchtraceHU.log "E:\dev\Luisetta\cfg\StatusQuo.dms" /runs/Hungary/endstate/GeneratedFiles /runs/Hungary/simulation_results/landuse/GridData
-rem "C:\Program Files\ObjectVision\GeoDms7207\GeoDmsRun.exe" /LbatchtraceIE.log "E:\dev\Luisetta\cfg\StatusQuo.dms" /runs/Ireland/endstate/GeneratedFiles /runs/Ireland/simulation_results/landuse/GridData
-rem "C:\Program Files\ObjectVision\GeoDms7207\GeoDmsRun.exe" /LbatchtraceIT.log "E:\dev\Luisetta\cfg\StatusQuo.dms" /runs/Italy/endstate/GeneratedFiles /runs/Italy/simulation_results/landuse/GridData
-rem "C:\Program Files\ObjectVision\GeoDms7207\GeoDmsRun.exe" /LbatchtraceLV.log "E:\dev\Luisetta\cfg\StatusQuo.dms" /runs/Latvia/endstate/GeneratedFiles /runs/Latvia/simulation_results/landuse/GridData
-rem "C:\Program Files\ObjectVision\GeoDms7207\GeoDmsRun.exe" /LbatchtraceLT.log "E:\dev\Luisetta\cfg\StatusQuo.dms" /runs/Lithuania/endstate/GeneratedFiles /runs/Lithuania/simulation_results/landuse/GridData
-rem "C:\Program Files\ObjectVision\GeoDms7207\GeoDmsRun.exe" /LbatchtraceMT.log "E:\dev\Luisetta\cfg\StatusQuo.dms" /runs/Malta/endstate/GeneratedFiles /runs/Malta/simulation_results/landuse/GridData
-rem "C:\Program Files\ObjectVision\GeoDms7207\GeoDmsRun.exe" /LbatchtraceNL.log "E:\dev\Luisetta\cfg\StatusQuo.dms" /runs/Netherlands/endstate/GeneratedFiles /runs/Netherlands/simulation_results/landuse/GridData
-rem "C:\Program Files\ObjectVision\GeoDms7207\GeoDmsRun.exe" /LbatchtracePL.log "E:\dev\Luisetta\cfg\StatusQuo.dms" /runs/Poland/endstate/GeneratedFiles /runs/Poland/simulation_results/landuse/GridData
-rem "C:\Program Files\ObjectVision\GeoDms7207\GeoDmsRun.exe" /LbatchtracePT.log "E:\dev\Luisetta\cfg\StatusQuo.dms" /runs/Portugal/endstate/GeneratedFiles /runs/Portugal/simulation_results/landuse/GridData
-rem "C:\Program Files\ObjectVision\GeoDms7207\GeoDmsRun.exe" /LbatchtraceRO.log "E:\dev\Luisetta\cfg\StatusQuo.dms" /runs/Romania/endstate/GeneratedFiles /runs/Romania/simulation_results/landuse/GridData
-rem "C:\Program Files\ObjectVision\GeoDms7207\GeoDmsRun.exe" /LbatchtraceSK.log "E:\dev\Luisetta\cfg\StatusQuo.dms" /runs/Slovakia/endstate/GeneratedFiles /runs/Slovakia/simulation_results/landuse/GridData
-rem "C:\Program Files\ObjectVision\GeoDms7207\GeoDmsRun.exe" /LbatchtraceSI.log "E:\dev\Luisetta\cfg\StatusQuo.dms" /runs/Slovenia/endstate/GeneratedFiles /runs/Slovenia/simulation_results/landuse/GridData
-rem "C:\Program Files\ObjectVision\GeoDms7207\GeoDmsRun.exe" /LbatchtraceES.log "E:\dev\Luisetta\cfg\StatusQuo.dms" /runs/Spain/endstate/GeneratedFiles /runs/Spain/simulation_results/landuse/GridData
-rem "C:\Program Files\ObjectVision\GeoDms7207\GeoDmsRun.exe" /LbatchtraceSE.log "E:\dev\Luisetta\cfg\StatusQuo.dms" /runs/Sweden/endstate/GeneratedFiles /runs/Sweden/simulation_results/landuse/GridData
-rem "C:\Program Files\ObjectVision\GeoDms7207\GeoDmsRun.exe" /LbatchtraceUK.log "E:\dev\Luisetta\cfg\StatusQuo.dms" /runs/United_Kingdom/endstate/GeneratedFiles /runs/United_Kingdom/simulation_results/landuse/GridData
+set ProgramPath=C:\Program Files\ObjectVision\%geodmsversion%\GeoDmsRun.exe
 
-"C:\Program Files\ObjectVision\GeoDms7207\GeoDmsRun.exe" /LbatchtraceALL.log "E:\dev\Luisetta\cfg\StatusQuo.dms"  /GenerateContainer/MozaikLandUse/impl/AllPeriods/P2050/result
+rem "%ProgramPath%" /LbatchtraceAT.log "E:\dev\Luisetta\cfg\StatusQuo.dms" /runs/Austria/endstate/GeneratedFiles /runs/Austria/simulation_results/landuse/GridData
+rem "%ProgramPath%" /LbatchtraceBL.log "E:\dev\Luisetta\cfg\StatusQuo.dms" /runs/BelgLux/endstate/GeneratedFiles /runs/BelLux/simulation_results/landuse/GridData
+ "%ProgramPath%" /LbatchtraceBG.log "E:\dev\Luisetta\cfg\StatusQuo.dms" /runs/Bulgaria/endstate/GeneratedFiles /runs/Bulgaria/simulation_results/landuse/GridData
+ "%ProgramPath%" /LbatchtraceCY.log "E:\dev\Luisetta\cfg\StatusQuo.dms" /runs/Cyprus/endstate/GeneratedFiles /runs/Cyprus/simulation_results/landuse/GridData
+ "%ProgramPath%" /LbatchtraceHR.log "E:\dev\Luisetta\cfg\StatusQuo.dms" /runs/Croatia/endstate/GeneratedFiles /runs/Croatia/simulation_results/landuse/GridData
+ "%ProgramPath%" /LbatchtraceCZ.log "E:\dev\Luisetta\cfg\StatusQuo.dms" /runs/Czechia/endstate/GeneratedFiles /runs/Czechia/simulation_results/landuse/GridData
+ "%ProgramPath%" /LbatchtraceDK.log "E:\dev\Luisetta\cfg\StatusQuo.dms" /runs/Denmark/endstate/GeneratedFiles /runs/Denmark/simulation_results/landuse/GridData
+ "%ProgramPath%" /LbatchtraceEE.log "E:\dev\Luisetta\cfg\StatusQuo.dms" /runs/Estonia/endstate/GeneratedFiles /runs/Estonia/simulation_results/landuse/GridData
+ "%ProgramPath%" /LbatchtraceFI.log "E:\dev\Luisetta\cfg\StatusQuo.dms" /runs/Finland/endstate/GeneratedFiles /runs/Finland/simulation_results/landuse/GridData
+ "%ProgramPath%" /LbatchtraceFR.log "E:\dev\Luisetta\cfg\StatusQuo.dms" /runs/France/endstate/GeneratedFiles /runs/France/simulation_results/landuse/GridData
+ "%ProgramPath%" /LbatchtraceDE.log "E:\dev\Luisetta\cfg\StatusQuo.dms" /runs/Germany/endstate/GeneratedFiles /runs/Germany/simulation_results/landuse/GridData
+ "%ProgramPath%" /LbatchtraceEL.log "E:\dev\Luisetta\cfg\StatusQuo.dms" /runs/Greece/endstate/GeneratedFiles /runs/Greece/simulation_results/landuse/GridData
+ "%ProgramPath%" /LbatchtraceHU.log "E:\dev\Luisetta\cfg\StatusQuo.dms" /runs/Hungary/endstate/GeneratedFiles /runs/Hungary/simulation_results/landuse/GridData
+ "%ProgramPath%" /LbatchtraceIE.log "E:\dev\Luisetta\cfg\StatusQuo.dms" /runs/Ireland/endstate/GeneratedFiles /runs/Ireland/simulation_results/landuse/GridData
+ "%ProgramPath%" /LbatchtraceIT.log "E:\dev\Luisetta\cfg\StatusQuo.dms" /runs/Italy/endstate/GeneratedFiles /runs/Italy/simulation_results/landuse/GridData
+ "%ProgramPath%" /LbatchtraceLV.log "E:\dev\Luisetta\cfg\StatusQuo.dms" /runs/Latvia/endstate/GeneratedFiles /runs/Latvia/simulation_results/landuse/GridData
+ "%ProgramPath%" /LbatchtraceLT.log "E:\dev\Luisetta\cfg\StatusQuo.dms" /runs/Lithuania/endstate/GeneratedFiles /runs/Lithuania/simulation_results/landuse/GridData
+ "%ProgramPath%" /LbatchtraceMT.log "E:\dev\Luisetta\cfg\StatusQuo.dms" /runs/Malta/endstate/GeneratedFiles /runs/Malta/simulation_results/landuse/GridData
+ "%ProgramPath%" /LbatchtraceNL.log "E:\dev\Luisetta\cfg\StatusQuo.dms" /runs/Netherlands/endstate/GeneratedFiles /runs/Netherlands/simulation_results/landuse/GridData
+ "%ProgramPath%" /LbatchtracePL.log "E:\dev\Luisetta\cfg\StatusQuo.dms" /runs/Poland/endstate/GeneratedFiles /runs/Poland/simulation_results/landuse/GridData
+ "%ProgramPath%" /LbatchtracePT.log "E:\dev\Luisetta\cfg\StatusQuo.dms" /runs/Portugal/endstate/GeneratedFiles /runs/Portugal/simulation_results/landuse/GridData
+ "%ProgramPath%" /LbatchtraceRO.log "E:\dev\Luisetta\cfg\StatusQuo.dms" /runs/Romania/endstate/GeneratedFiles /runs/Romania/simulation_results/landuse/GridData
+ "%ProgramPath%" /LbatchtraceSK.log "E:\dev\Luisetta\cfg\StatusQuo.dms" /runs/Slovakia/endstate/GeneratedFiles /runs/Slovakia/simulation_results/landuse/GridData
+ "%ProgramPath%" /LbatchtraceSI.log "E:\dev\Luisetta\cfg\StatusQuo.dms" /runs/Slovenia/endstate/GeneratedFiles /runs/Slovenia/simulation_results/landuse/GridData
+ "%ProgramPath%" /LbatchtraceES.log "E:\dev\Luisetta\cfg\StatusQuo.dms" /runs/Spain/endstate/GeneratedFiles /runs/Spain/simulation_results/landuse/GridData
+ "%ProgramPath%" /LbatchtraceSE.log "E:\dev\Luisetta\cfg\StatusQuo.dms" /runs/Sweden/endstate/GeneratedFiles /runs/Sweden/simulation_results/landuse/GridData
+ "%ProgramPath%" /LbatchtraceUK.log "E:\dev\Luisetta\cfg\StatusQuo.dms" /runs/United_Kingdom/endstate/GeneratedFiles /runs/United_Kingdom/simulation_results/landuse/GridData
+
+ "%ProgramPath%" /LbatchtraceALL.log "E:\dev\Luisetta\cfg\StatusQuo.dms"  /GenerateContainer/MozaikLandUse/impl/AllPeriods/P2050/result
