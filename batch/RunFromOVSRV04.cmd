ea47751cb3e1f28f9ec31bc685e522b64f857777
Maarten Hilferink <mhilferink@objectvision.nl>
2020-02-12 16:08:41 +0100

    issue 1287, batch files.

---
+++ batch/RunFromOVSRV04.cmd
@@ -0,0 +1,6 @@
+set geodmsversion=GeoDms7206
+set pf=%Program Files%
+set luisaBeesFolder=C:\prj\jrc\Luisetta
+set ProgramPath=C:\Program Files\ObjectVision\%geodmsversion%\GeoDmsRun.exe
+
+call RunImpl.cmd
---
+++ batch/RunImpl.cmd
@@ -0,0 +1,3 @@
+"%ProgramPath%" /LbatchtraceGenerate.log  "%luisaBeesFolder%\cfg\StatusQuo.dms" /GenerateContainer/CalcRunRegions/ButtonCan/Results
+"%ProgramPath%" /LbatchtraceMozaikLU.log  "%luisaBeesFolder%\cfg\StatusQuo.dms" /GenerateContainer/MozaikLandUse/ButtonCan/Generate
+"%ProgramPath%" /LbatchtraceMozaikPOP.log "%luisaBeesFolder%\cfg\StatusQuo.dms" /GenerateContainer/MozaikPopulation/ButtonCan/Generate
