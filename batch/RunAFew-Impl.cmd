a722ca3c57b250b592322d15cb1648e3ab7e9d98
Maarten Hilferink <mhilferink@objectvision.nl>
2021-02-11 09:23:34 +0100

    Export shadow_prices for Capri claimed land use to LD/Results/Country/Pyyyy/CapriShadowPrices.csv

--- batch/RunAFew-Impl.cmd
+++ batch/RunAFew-Impl.cmd
@@ -1,34 +1,35 @@
-set geodmsversion=GeoDms7206
+set geodmsversion=GeoDms7305
 set pf=%Program Files%
 set luisetta=C:\luisetta
 set ProgramPath=C:\Program Files\ObjectVision\%geodmsversion%\GeoDmsRun.exe
 
-rem "%ProgramPath%" /LbatchtraceAT.log "C:\luisabees\luisetta\cfg\StatusQuo.dms" /runs/Austria/endstate/GeneratedFiles /runs/Austria/simulation_results/landuse/GridData
-rem "%ProgramPath%" /LbatchtraceBL.log "C:\luisabees\luisetta\cfg\StatusQuo.dms" /runs/BelgLux/endstate/GeneratedFiles /runs/BelLux/simulation_results/landuse/GridData
- rem "%ProgramPath%" /LbatchtraceBG.log "C:\luisabees\luisetta\cfg\StatusQuo.dms" /runs/Bulgaria/endstate/GeneratedFiles /runs/Bulgaria/simulation_results/landuse/GridData
- rem "%ProgramPath%" /LbatchtraceCY.log "C:\luisabees\luisetta\cfg\StatusQuo.dms" /runs/Cyprus/endstate/GeneratedFiles /runs/Cyprus/simulation_results/landuse/GridData
- rem "%ProgramPath%" /LbatchtraceHR.log "C:\luisabees\luisetta\cfg\StatusQuo.dms" /runs/Croatia/endstate/GeneratedFiles /runs/Croatia/simulation_results/landuse/GridData
-rem  "%ProgramPath%" /LbatchtraceCZ.log "C:\luisabees\luisetta\cfg\StatusQuo.dms" /runs/Czechia/endstate/GeneratedFiles /runs/Czechia/simulation_results/landuse/GridData
-rem  "%ProgramPath%" /LbatchtraceDK.log "C:\luisabees\luisetta\cfg\StatusQuo.dms" /runs/Denmark/endstate/GeneratedFiles /runs/Denmark/simulation_results/landuse/GridData
- rem "%ProgramPath%" /LbatchtraceEE.log "C:\luisabees\luisetta\cfg\StatusQuo.dms" /runs/Estonia/endstate/GeneratedFiles /runs/Estonia/simulation_results/landuse/GridData
-rem  "%ProgramPath%" /LbatchtraceFI.log "C:\luisabees\luisetta\cfg\StatusQuo.dms" /runs/Finland/endstate/GeneratedFiles /runs/Finland/simulation_results/landuse/GridData
-rem  "%ProgramPath%" /LbatchtraceFR.log "C:\luisabees\luisetta\cfg\StatusQuo.dms" /runs/France/endstate/GeneratedFiles /runs/France/simulation_results/landuse/GridData
-rem  "%ProgramPath%" /LbatchtraceDE.log "C:\luisabees\luisetta\cfg\StatusQuo.dms" /runs/Germany/endstate/GeneratedFiles /runs/Germany/simulation_results/landuse/GridData
-rem  "%ProgramPath%" /LbatchtraceEL.log "C:\luisabees\luisetta\cfg\StatusQuo.dms" /runs/Greece/endstate/GeneratedFiles /runs/Greece/simulation_results/landuse/GridData
- rem "%ProgramPath%" /LbatchtraceHU.log "C:\luisabees\luisetta\cfg\StatusQuo.dms" /runs/Hungary/endstate/GeneratedFiles /runs/Hungary/simulation_results/landuse/GridData
- rem "%ProgramPath%" /LbatchtraceIE.log "C:\luisabees\luisetta\cfg\StatusQuo.dms" /runs/Ireland/endstate/GeneratedFiles /runs/Ireland/simulation_results/landuse/GridData
- rem "%ProgramPath%" /LbatchtraceIT.log "C:\luisabees\luisetta\cfg\StatusQuo.dms" /runs/Italy/endstate/GeneratedFiles /runs/Italy/simulation_results/landuse/GridData
-rem  "%ProgramPath%" /LbatchtraceLV.log "C:\luisabees\luisetta\cfg\StatusQuo.dms" /runs/Latvia/endstate/GeneratedFiles /runs/Latvia/simulation_results/landuse/GridData
-rem  "%ProgramPath%" /LbatchtraceLT.log "C:\luisabees\luisetta\cfg\StatusQuo.dms" /runs/Lithuania/endstate/GeneratedFiles /runs/Lithuania/simulation_results/landuse/GridData
- rem "%ProgramPath%" /LbatchtraceMT.log "C:\luisabees\luisetta\cfg\StatusQuo.dms" /runs/Malta/endstate/GeneratedFiles /runs/Malta/simulation_results/landuse/GridData
- rem "%ProgramPath%" /LbatchtraceNL.log "C:\luisabees\luisetta\cfg\StatusQuo.dms" /runs/Netherlands/endstate/GeneratedFiles /runs/Netherlands/simulation_results/landuse/GridData
- rem "%ProgramPath%" /LbatchtracePL.log "C:\luisabees\luisetta\cfg\StatusQuo.dms" /runs/Poland/endstate/GeneratedFiles /runs/Poland/simulation_results/landuse/GridData
- rem "%ProgramPath%" /LbatchtracePT.log "C:\luisabees\luisetta\cfg\StatusQuo.dms" /runs/Portugal/endstate/GeneratedFiles /runs/Portugal/simulation_results/landuse/GridData
-rem  "%ProgramPath%" /LbatchtraceRO.log "C:\luisabees\luisetta\cfg\StatusQuo.dms" /runs/Romania/endstate/GeneratedFiles /runs/Romania/simulation_results/landuse/GridData
- rem "%ProgramPath%" /LbatchtraceSK.log "C:\luisabees\luisetta\cfg\StatusQuo.dms" /runs/Slovakia/endstate/GeneratedFiles /runs/Slovakia/simulation_results/landuse/GridData
- "%ProgramPath%" /LbatchtraceSI.log "C:\luisabees\luisetta\cfg\StatusQuo.dms" /runs/Slovenia/endstate/GeneratedFiles /runs/Slovenia/simulation_results/landuse/GridData
- "%ProgramPath%" /LbatchtraceES.log "C:\luisabees\luisetta\cfg\StatusQuo.dms" /runs/Spain/endstate/GeneratedFiles /runs/Spain/simulation_results/landuse/GridData
- "%ProgramPath%" /LbatchtraceSE.log "C:\luisabees\luisetta\cfg\StatusQuo.dms" /runs/Sweden/endstate/GeneratedFiles /runs/Sweden/simulation_results/landuse/GridData
- "%ProgramPath%" /LbatchtraceUK.log "C:\luisabees\luisetta\cfg\StatusQuo.dms" /runs/United_Kingdom/endstate/GeneratedFiles /runs/United_Kingdom/simulation_results/landuse/GridData
+rem "%ProgramPath%" /LbatchtraceAT.log "C:\luisabees\luisetta\cfg\StatusQuo.dms" /runs/Austria/endstate/GeneratedFiles /runs/Austria/simulation_results/GeneratedFiles
+rem "%ProgramPath%" /LbatchtraceBL.log "C:\luisabees\luisetta\cfg\StatusQuo.dms" /runs/BelgLux/endstate/GeneratedFiles /runs/BelLux/simulation_results/GeneratedFiles
+rem "%ProgramPath%" /LbatchtraceBG.log "C:\luisabees\luisetta\cfg\StatusQuo.dms" /runs/Bulgaria/endstate/GeneratedFiles /runs/Bulgaria/simulation_results/GeneratedFiles
+rem "%ProgramPath%" /LbatchtraceCY.log "C:\luisabees\luisetta\cfg\StatusQuo.dms" /runs/Cyprus/endstate/GeneratedFiles /runs/Cyprus/simulation_results/GeneratedFiles
+rem "%ProgramPath%" /LbatchtraceHR.log "C:\luisabees\luisetta\cfg\StatusQuo.dms" /runs/Croatia/endstate/GeneratedFiles /runs/Croatia/simulation_results/GeneratedFiles
+rem "%ProgramPath%" /LbatchtraceCZ.log "C:\luisabees\luisetta\cfg\StatusQuo.dms" /runs/Czechia/endstate/GeneratedFiles /runs/Czechia/simulation_results/GeneratedFiles
+  rem "%ProgramPath%" /LbatchtraceDK.log "C:\luisabees\luisetta\cfg\StatusQuo.dms" /runs/Denmark/endstate/GeneratedFiles /runs/Denmark/simulation_results/GeneratedFiles
+rem "%ProgramPath%" /LbatchtraceEE.log "C:\luisabees\luisetta\cfg\StatusQuo.dms" /runs/Estonia/endstate/GeneratedFiles /runs/Estonia/simulation_results/GeneratedFiles
+rem "%ProgramPath%" /LbatchtraceFI.log "C:\luisabees\luisetta\cfg\StatusQuo.dms" /runs/Finland/endstate/GeneratedFiles /runs/Finland/simulation_results/GeneratedFiles
+rem "%ProgramPath%" /LbatchtraceFR.log "C:\luisabees\luisetta\cfg\StatusQuo.dms" /runs/France/endstate/GeneratedFiles /runs/France/simulation_results/GeneratedFiles
+rem "%ProgramPath%" /LbatchtraceDE.log "C:\luisabees\luisetta\cfg\StatusQuo.dms" /runs/Germany/endstate/GeneratedFiles /runs/Germany/simulation_results/GeneratedFiles
+rem "%ProgramPath%" /LbatchtraceEL.log "C:\luisabees\luisetta\cfg\StatusQuo.dms" /runs/Greece/endstate/GeneratedFiles /runs/Greece/simulation_results/GeneratedFiles
+rem "%ProgramPath%" /LbatchtraceHU.log "C:\luisabees\luisetta\cfg\StatusQuo.dms" /runs/Hungary/endstate/GeneratedFiles /runs/Hungary/simulation_results/GeneratedFiles
+rem "%ProgramPath%" /LbatchtraceIE.log "C:\luisabees\luisetta\cfg\StatusQuo.dms" /runs/Ireland/endstate/GeneratedFiles /runs/Ireland/simulation_results/GeneratedFiles
+rem "%ProgramPath%" /LbatchtraceIT.log "C:\luisabees\luisetta\cfg\StatusQuo.dms" /runs/Italy/endstate/GeneratedFiles /runs/Italy/simulation_results/GeneratedFiles
+rem "%ProgramPath%" /LbatchtraceLV.log "C:\luisabees\luisetta\cfg\StatusQuo.dms" /runs/Latvia/endstate/GeneratedFiles /runs/Latvia/simulation_results/GeneratedFiles
+rem "%ProgramPath%" /LbatchtraceLT.log "C:\luisabees\luisetta\cfg\StatusQuo.dms" /runs/Lithuania/endstate/GeneratedFiles /runs/Lithuania/simulation_results/GeneratedFiles
+rem "%ProgramPath%" /LbatchtraceMT.log "C:\luisabees\luisetta\cfg\StatusQuo.dms" /runs/Malta/endstate/GeneratedFiles /runs/Malta/simulation_results/GeneratedFiles
+  "%ProgramPath%" /LbatchtraceNL.log "C:\luisabees\luisetta\cfg\StatusQuo.dms" /runs/Netherlands/endstate/GeneratedFiles /runs/Netherlands/simulation_results/GeneratedFiles
+rem "%ProgramPath%" /LbatchtracePL.log "C:\luisabees\luisetta\cfg\StatusQuo.dms" /runs/Poland/endstate/GeneratedFiles /runs/Poland/simulation_results/GeneratedFiles
+rem "%ProgramPath%" /LbatchtracePT.log "C:\luisabees\luisetta\cfg\StatusQuo.dms" /runs/Portugal/endstate/GeneratedFiles /runs/Portugal/simulation_results/GeneratedFiles
+rem "%ProgramPath%" /LbatchtraceRO.log "C:\luisabees\luisetta\cfg\StatusQuo.dms" /runs/Romania/endstate/GeneratedFiles /runs/Romania/simulation_results/GeneratedFiles
+rem "%ProgramPath%" /LbatchtraceSK.log "C:\luisabees\luisetta\cfg\StatusQuo.dms" /runs/Slovakia/endstate/GeneratedFiles /runs/Slovakia/simulation_results/GeneratedFiles
 
- "%ProgramPath%" /LbatchtraceALL.log "C:\luisabees\luisetta\cfg\StatusQuo.dms"  /GenerateContainer/MozaikLandUse/impl/AllPeriods/P2050/result
+  rem  "%ProgramPath%" /LbatchtraceSI.log "C:\luisabees\luisetta\cfg\StatusQuo.dms" /runs/Slovenia/endstate/GeneratedFiles /runs/Slovenia/simulation_results/GeneratedFiles
+rem  "%ProgramPath%" /LbatchtraceES.log "C:\luisabees\luisetta\cfg\StatusQuo.dms" /runs/Spain/endstate/GeneratedFiles /runs/Spain/simulation_results/GeneratedFiles
+rem  "%ProgramPath%" /LbatchtraceSE.log "C:\luisabees\luisetta\cfg\StatusQuo.dms" /runs/Sweden/endstate/GeneratedFiles /runs/Sweden/simulation_results/GeneratedFiles
+rem  "%ProgramPath%" /LbatchtraceUK.log "C:\luisabees\luisetta\cfg\StatusQuo.dms" /runs/United_Kingdom/endstate/GeneratedFiles /runs/United_Kingdom/simulation_results/GeneratedFiles
+
+rem  "%ProgramPath%" /LbatchtraceALL.log "C:\luisabees\luisetta\cfg\StatusQuo.dms"  /GenerateContainer/MozaikLandUse/impl/AllPeriods/P2050/result
