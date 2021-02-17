1e550400de920f6755198455e8589c72ea0dc758
Maarten Hilferink <mhilferink@objectvision.nl>
2021-02-17 11:59:18 +0100

    Capri variations: extra batch file.

---
+++ batch/RunCapriVariants.cmd
@@ -0,0 +1,17 @@
+set DIR_CAPRI=CAPRI_01
+del "C:\LD2\luisa-bees\CalcCachex64.v7.306" /S /Q
+call RunAFew-Impl.cmd
+
+set DIR_CAPRI=CAPRI_05
+del "C:\LD2\luisa-bees\CalcCachex64.v7.306" /S /Q
+call RunAFew-Impl.cmd
+
+set DIR_CAPRI=CAPRI_10
+del "C:\LD2\luisa-bees\CalcCachex64.v7.306" /S /Q
+call RunAFew-Impl.cmd
+
+set DIR_CAPRI=CAPRI
+del "C:\LD2\luisa-bees\CalcCachex64.v7.306" /S /Q
+call RunAFew-Impl.cmd
+
+pause
