9e6f0984a032e096b42fd6e9206e2cdd685afe97
Maarten Hilferink <mhilferink@objectvision.nl>
2021-04-30 12:29:13 +0200

    Minor changes to the production and merging of CapriVariants in order to produce DK, and NL for AW10

--- batch/RunCapriVariants.cmd
+++ batch/RunCapriVariants.cmd
@@ -3,6 +3,8 @@ call RunAFewAI.cmd
 pause 8*11*25*1
 exit /B
 
+set AW=00
+call RunAFewAI.cmd
 set AW=05
 call RunAFewAI.cmd
 set AW=10
@@ -12,4 +14,4 @@ call RunAFewAI.cmd
 REM set AW=20
 REM call RunAFewAI.cmd
 
-pause 8*11*25*5
+pause 8*11*25*4
